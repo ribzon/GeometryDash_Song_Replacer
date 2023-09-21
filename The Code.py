20c5d62ec373fffcea0c4ac92d84af0a5d1f9062
Shocoo <114026653+Shocoo@users.noreply.github.com>
2023-09-21 15:44:27 +0200

    Updated the code
    Added :
    - A system that doesn't download the background every time
    - The code checks if the bg url has been updated, so you don't have to remove manually the downloaded one
    - Animated bg !!

--- The Code.py
+++ The Code.py
@@ -8,16 +8,32 @@ import requests
 import tempfile
 from io import BytesIO
 import shutil  # Import the shutil library for file copying
+import threading # To run parallel tasks, it's just some optimization
+
 
 class FileReplacerGUI:
+  
     def __init__(self, master):
         self.master = master
-        master.title("File Replacer")
-        master.geometry('1000x700')
+        master.title("Geometry Dash song replacer")
+        master.geometry('640x360')
         master.resizable(False, False)  # Lock the window size
 
+        self.image_path = os.path.join(tempfile.gettempdir(), 'anim.gif')
+
+
+        self.gif_frames = []
+        self.gif_frame_dur = 0
+        self.gif_frame_count = -1
+        self.background_image = None
+
+        gif_lb = tk.Label(self.master, image=self.background_image)
+        gif_lb.pack(fill=tk.BOTH)
+        self.gif_label = gif_lb
+
+
         # Download and set background image
-        background_url = "https://cdn.discordapp.com/attachments/1026244641370144899/1149982530192285766/image456.png"
+        background_url = "https://media.tenor.com/ljZfH3ZkEqEAAAAd/cherry-blossom.gif"
         self.set_background_from_url(background_url)
 
         self.current_file_path = ""
@@ -40,7 +56,7 @@ class FileReplacerGUI:
         self.execute_button.pack(pady=20)
 
         # Load and display the Discord icon image
-        discord_icon_url = "https://cdn.discordapp.com/attachments/1099374101119905902/1149976767860314132/bmc.png"
+        discord_icon_url = "https://cdn.discordapp.com/attachments/1026244641370144899/1153770135673376910/bmc.png"
         image = self.load_image_from_url(discord_icon_url)
 
         if image:
@@ -63,22 +79,71 @@ class FileReplacerGUI:
             return None
 
     def set_background_from_url(self, url):
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                image_data = response.content
-                image_path = os.path.join(tempfile.gettempdir(), 'background_image.jpg')
-                with open(image_path, 'wb') as image_file:
-                    image_file.write(image_data)
-                self.master.configure(bg='black')
-                self.background_image = ImageTk.PhotoImage(Image.open(image_path))
-                background_label = tk.Label(self.master, image=self.background_image)
-                background_label.place(relwidth=1, relheight=1)
-            else:
-                print(f"Failed to download background image from URL: {url}")
-        except Exception as e:
-            print(f"Failed to set background image: {e}")
-            return None
+
+        #The url.txt keeps the url of the downloaded background, so if the background download link is updated,
+        # the background stored on the system will get replaced.
+
+        url_path = os.path.join(tempfile.gettempdir(), 'url.txt')
+
+        #Verify if the bg is already downloaded and up-to-date, else it gets updated.
+        if os.path.exists(self.image_path) and os.path.exists(url_path) and url == open(url_path, 'r').read():
+            print("No need to download anything")
+            pass
+        else:
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    image_data = response.content
+                    with open(self.image_path, 'wb') as image_file:
+                        image_file.write(image_data)
+                    with open(url_path, 'w') as url_file:
+                        url_file.write(url)
+                    print("Background downloaded !")
+                else:
+                    print(f"Failed to download background image from URL: {url}")
+            except Exception as e:
+                print(f"Failed to set background image: {e}")
+                print()
+                print('Check your internet connection !')
+                print()
+                return None
+            
+        #Display the bg
+
+        #self.prepare_gif() instead of the next line if you want the window to launch only when the bg is ready
+
+        threading.Thread(target=self.prepare_gif).start()
+            
+    def prepare_gif(self):
+
+        #Open gif file
+        gif_file = Image.open(self.image_path)
+
+        #Store frames in gif_frames and duration in gif_frame_dur
+        for i in range(0, gif_file.n_frames):
+            gif_file.seek(i)
+            self.gif_frames.append(gif_file.copy())
+        self.gif_frame_dur = gif_file.info['duration']
+
+        self.play_gif()
+
+    def play_gif(self):
+
+        #If it's on the last frame, then go to the first
+        if self.gif_frame_count >= len(self.gif_frames) -1:
+            self.gif_frame_count = -1
+
+        self.gif_frame_count += 1
+
+        #Display current frame
+
+        self.background_image = ImageTk.PhotoImage(self.gif_frames[self.gif_frame_count])
+        self.gif_label.config(image=self.background_image)
+        self.gif_label.place(relwidth=1, relheight =1)
+
+        #Add a delay for the next frame to be displayed
+
+        self.master.after(self.gif_frame_dur, self.play_gif)
 
     def select_current_file(self):
         # Open a file dialog to select the file to replace
@@ -121,6 +186,6 @@ class FileReplacerGUI:
         webbrowser.open("https://discord.gg/nfyVQ762XM")
 
 # Create the GUI
-root = tk.Tk()
+root = tk.Tk()   
 file_replacer_gui = FileReplacerGUI(root)
 root.mainloop()
